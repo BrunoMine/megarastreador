5822bb88edb2fc44e68f7c5f5b29ef53f6e1a48d
Lunovox <rui.gravata@gmail.com>
2015-02-26 21:21:49 -0300

    Nova forma de echo em terminal

--- init.lua
+++ init.lua
@@ -10,13 +10,9 @@ Git de colaboração e desenvolvimento <>
 -- Inicializando mod
 -- 
 
-print("[MegaRastreador] Carregando mod MegaRastreador... ")
 dofile(minetest.get_modpath("megarastreador").."/diretrizes.lua")
-print("[MegaRastreador] Diretrizes carregadas... ")
 dofile(minetest.get_modpath("megarastreador").."/funcoes.lua")
-print("[MegaRastreador] Funcoes carregados... ")
 dofile(minetest.get_modpath("megarastreador").."/node.lua")
-print("[MegaRastreador] Laptop carregado... ")
 dofile(minetest.get_modpath("megarastreador").."/receitas.lua")
-print("[MegaRastreador] Receitas carregadas... ")
-print("[MegaRastreador] OK")
+
+minetest.log('action',"[MEGARASTREADOR] Carregado...")
